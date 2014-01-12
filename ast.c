5afc89d20adeec62d99d4da1c9c1d9fe19724139
以俊德 <826531041@qq.com>
2014-01-12 16:11:38 +0800

    little changes for argv[]
    Thanks for @滚来滚去!

--- ast.c
+++ ast.c
@@ -1,20 +1,37 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-int main ( int argc, char* argv[] )
+int main ( int argc, char *argv[] )
 {
-	if (argc == 1)
-	{
-		puts("Anthon-Starter 0.x.x-Dev  Copyright (C) 2014 AOSC-JDS");
-		puts("\nUsage: ast <command> <image_file> <install_route> [<install_mode>...]");
-		puts("\n<Commands>\n");
-		puts("  i: Install Anthon GNU/Linux to the computer\n  c: Check the system and put them into .\\info.ast\n  v: Verify the image file\n  a: show About information");
-		puts("\nNotes:");
-		puts("  1. lalal\n  2. alala\n  3. Just a test!\n");
-	}
 	/*printf("\n%d", argc);
-	printf("\nName: %s", argv[0]);
-	printf("\nImage file is: %s", argv[1]);
-	printf("\nLocation: %s\n", argv[2]);*/
+	printf("\n%s", argv[0]);
+	printf("\n%s\n", argv[1]);*/
+	if (argc > 1)
+	{
+		if (strcmp(argv[1], "install") == 0)
+		{
+			puts("We haven\'t finished it yet!");
+			exit(0);
+		}
+		
+		if (strcmp(argv[1], "help") == 0)
+		{
+			puts("We haven\'t finished it yet!");
+			exit(0);
+		}
+		
+		if (strcmp(argv[1], "about") == 0)
+		{
+			puts("We haven\'t finished it yet!");
+			exit(0);
+		}
+	}
+	puts("Anthon-Starter 0.x.x-Dev  Copyright (C) 2014 AOSC-JDS");
+	puts("\nUsage: ast <command> <image_file> <install_route> [<install_mode>...]");
+	puts("\n<Commands>\n");
+	puts("  install: Install Anthon GNU/Linux to the computer\n  help: Show the help\n  about: Show bbout information");
+	puts("\nNotes:");
+	puts("  1. lalal\n  2. alala\n  3. Just a test!\n");
+
 	return 0;
 }
