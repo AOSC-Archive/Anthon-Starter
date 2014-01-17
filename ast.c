7ae6136a7d3e7b4e6aa592d0a560369fdecc3a30
以俊德 <826531041@qq.com>
2014-01-18 01:25:17 +0800

    Update 20140118
    +function: extract&deploy; +switches detecting; +problem:It's said line
    125 makes line 128 error. Thanks for @月光的指引~!

--- ast.c
+++ ast.c
@@ -1,29 +1,78 @@
 # include <stdio.h>
+# include <io.h>
 # include <stdlib.h>
+# include <string.h>
 
 void help_message( void );
-void check( char *img[], char *tgt[] );
-int i;
+void check( char **img, char **tgt );
+int extract ( char **img, char **tgt );
+int deploy ( char **tgt );
+int i, tmp;
 
-int main ( int argc, char *argv[] )
+int main ( int argc, char **argv )
 {
-	printf("\n==========Testing area");
+	int noverify = 0, stepping = 0, verbose = 0, reboot = 0, quiet = 0;
+	
+	printf("\nAnthon-Starter 0.2.0-Dev  Copyright (C) 2014 AOSC-JDS\n");
+	printf("\n==========Execute Information==========");
 	printf("\nCompile Time: %s %s\nGCC Version: %s\n", __DATE__, __TIME__, __VERSION__);
 	printf("%d Parameters detected.\n", argc-1);
 	for( i = 0; i<argc; i++)
 		printf("%s is argv[%d]\n", argv[i], i);
-	printf("======================\n");
-	
-	/*printf("%ld %ld", &argv[2], &argv[3]);*/
-	/*puts(argv[2]);*/
-	
+	printf("=======================================\n");
 	if ( argc > 1)
 	{
 		if (strcmp(argv[1], "install") == 0)
-		{
+		{	
+			for ( i = 3; i<argc; i++ )
+			{
+				if (strcmp(argv[i], "--no-verify") == 0)
+					noverify = 1;
+					
+				if (strcmp(argv[i], "--step-by-step") == 0)
+					stepping = 1;
+					
+				if (strcmp(argv[i], "--verbose") == 0)
+				{
+					if (quiet == 1)
+					{
+						printf("You cannot set verbose and quiet at the same time! Program exits.\n");
+						exit(1);
+					}
+					else verbose = 1;
+				}
+				
+				if (strcmp(argv[i], "--quiet") == 0)
+				{
+					if (verbose == 1)
+					{
+						printf("You cannot set verbose and quiet at the same time! Program exits.\n");
+						exit(1);
+					}
+					else quiet = 1;
+				}
+				
+				if (strcmp(argv[i], "--reboot") == 0)
+					reboot = 1;
+			}
+			
+			printf("noverify? %d; stepping? %d; verbose? %d; quiet? %d; reboot? %d;\n", noverify, stepping, verbose, quiet, reboot);
+			
+			
+			
 			check( &argv[2], &argv[3] );
-			printf("\nWe haven\'t finished it yet!\n");
-			exit(1);
+			if ( (extract( &argv[2], &argv[3] )) == 1)
+			{
+				printf("Error when extracting.\n");
+				exit(1);
+			}
+			if ( (deploy( &argv[3] )) == 1)
+			{
+				printf("Error when deploying.\n");
+				exit(1);
+			}
+			printf("Done.\n");
+			exit(0);
 		}
 		
 		if (strcmp(argv[1], "help") == 0)
@@ -38,7 +87,6 @@ int main ( int argc, char *argv[] )
 
 void help_message( void )
 {
-	printf("\nAnthon-Starter 0.2.0-Dev  Copyright (C) 2014 AOSC-JDS\n");
 	printf("\nUsage: ast <command> <image_file> <install_target> [<switches>...]\n");
 	printf("\n<Commands>\n");
 	printf("\tinstall: Install the specify AOSC distro to the computer\n\thelp: Show the help\n\tabout: Show bbout information");
@@ -48,7 +96,45 @@ void help_message( void )
 	printf("To report bugs please visit http://bugs.anthonos.org/\n");
 }
 
-void check( char *img[], char *tgt[] )
+void check( char **img, char **tgt )
 {
-	printf("%s %s", *img, *tgt );
+	printf("%s %s\n\n", *img, *tgt );
+	
+	if ( (access( *img, 00 )) == -1 )
+	{
+		printf("Cannot find the image file %s! Program exits.\n", *img);
+		exit(-1);
+	}
+	if ( (access( *tgt, 06)) == -1 )
+	{
+		printf("Cannot find %s or %s is not ready.\n", *tgt, *tgt);
+		exit(-1);
+	}
 }
+
+int extract ( char **img, char **tgt )
+{
+	char **t;
+	printf("Extracting Pre-Install Environment...\n");
+	
+	//So how to solve the problem of using malloc().
+	//Someone knows?
+	
+	/*t = malloc( 100 );
+	system("pause");
+	*t = "7z e ";
+	printf("%s\n", *t);
+	system("pause");
+	*t = strcat(*t, *img);
+	system("pause");
+	system(*t);*/
+	printf("Extracting nessesary OS files...\n");
+	return 0;
+}
+
+int deploy ( char **tgt )
+{
+	printf("Deploying bootloader...\n");
+	//AHHHHHHHHHH...
+}
+
