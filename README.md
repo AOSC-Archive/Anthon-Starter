a80139839a3af054c33bb3cf007397f4380b09ce
lmy441900 <826531041@qq.com>
2014-07-13 14:39:59 +0800

    Update 20140713
    Define the architecture of ast

--- README.md
+++ README.md
@@ -1,52 +1,5 @@
-Anthon-Starter
-==============
+Anthon-Starter 0.2.0
+====
 
-Installation helper for AOSC Linux distribution series.
+Installation helper for AOSC OSes, written with C.
 
-License
-==============
-
-Anthon-Starter is licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
-
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
-See the License for the specific language governing permissions and limitations under the License.
-
-Versions
-==============
-
-The next version written by Windows(R) Batch is 0.1.2 and the sources are in branch "master".
-
-**Notice: DO NOT run them because it must be ANSI but those here aren't.**
-
-Usage
-==============
-
-For 0.1.2:
-
-* Click twice to execute Anthon-Starter.
-* Follow the tips.
-* Wait for 3-5 minutes. Anthon-Starter will automatically finish the deploying work.
-* Then the LiveDVD system of Anthon GNU/Linux will start. You just need to execute the installer inside.
-
-Build
-==============
-
-To Build Anthon-Starter, you need:
-
-* Bat to Exe Converter
-* Microsoft(R) Windows(R) NT or later
-* CMD
-
-Remember to check if the source file is in ANSI! For more details, please visit http://wiki.anthonos.org/projects:anthon-starter:devel-guide
-
-Developers
-==============
-
-* Junde Yi <lmy441900@gmail.com>
-* Haoming Xu <18929292333@163.com>
-* Zixing Liu <liushuyu011@gmail.com> 
---
+++ funcs.h
@@ -0,0 +1,84 @@
+/*
+ * Anthon-Starter: Installation helper for AOSC OS series, version 0.2.0
+ * Copyright (C) 2014 Anthon Open Source Community
+ * This file is a part of Anthon-Starter, for function declaration using.
+ * 
+ * Anthon-Starter is licensed under GNU LGPL: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * Anthon-Starter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Anthon-Starter.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* int main ( int argc, char** argv, char** envp ); */
+
+/*
+ * chkargs: Check the arguments which is passed to Anthon-Starter.
+ */
+int chkargs ( int argc, char** argv );
+
+/*
+ * run: Including
+ *        - init
+ *        - getsysinfo
+ *        - backup
+ *        - extract
+ *        - verify
+ *        - deploy
+ *        - before_reboot
+ *      That means, it invokes the functions that run.
+ */
+int run ();
+
+/*
+ * init: Initialize Anthon-Starter.
+ */
+int init ();
+
+/*
+ * getsysinfo: Get the system info, such as system drive, CPU architecture, etc.
+ */
+int getsysinfo ( char** envp );
+
+/*
+ * backup: Backup the important files before we do everything.
+ */
+int backup ();
+
+/*
+ * extract: Extract necessary files to the specified destination, usually from the passed arguments.
+ */
+int extract ();
+
+/*
+ * verify: Verify the files that was extracted just now to ensure validity.
+ */
+int verify ();
+
+/*
+ * deploy: Deploy the boot loader.
+ */
+int deploy ();
+
+/*
+ * before_reboot: Release startup utility and do something necessary...
+ */
+int before_reboot ();
+
+/*
+ * help_message: Show help messages.
+ */
+int help_message ( void );
+
+/*
+ * startup: For startup use.
+ */
+int startup ();
