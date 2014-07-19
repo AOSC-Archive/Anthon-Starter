cdd758dc0d2ab07e4719cd54948dd456fc9b65d7
lmy441900 <826531041@qq.com>
2014-07-19 17:06:26 +0800

    Fixed License

--- chkargs.c
+++ chkargs.c
@@ -4,9 +4,9 @@
  * This file is a part of Anthon-Starter.
  * 
  * Anthon-Starter is licensed under GNU LGPL: you can redistribute it
- * and/or modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
  * 
  * Anthon-Starter is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,8 +14,8 @@
  * GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public
- * License along with Anthon-Starter.
- * If not, see <http://www.gnu.org/licenses/>.
+ * License along with Anthon-Starter. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 # include <stdio.h>
