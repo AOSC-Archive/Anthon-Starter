3850a158ccf53b32e95b2784b502f04e68c47c1a
liushuyu <liushuyu_011@163.com>
2014-08-29 16:20:32 +0800

    Added MBR backup code for testing.
    It is located in testing folder. And it is working perfectly unless you don't give it the admin privilege to do that :-).
    Just backup 512 bytes (1 sector)...

---
+++ testing/mbr_backup.c
@@ -0,0 +1,41 @@
+﻿// Test code: Read MBR and then write it into a file. 
+#define MBRBKFILE "C:\\mbrdata.bin" //where to store backupfile
+#include <windows.h> //OK, WINAPI again...:-)
+#include <tchar.h>
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+TCHAR szDevice[MAX_PATH] = _T("\\\\.\\PhysicalDrive0");//MBR is on sda0
+HANDLE hDevice = NULL;
+hDevice = CreateFile(szDevice,
+  GENERIC_READ, 
+  FILE_SHARE_READ | FILE_SHARE_WRITE,
+  NULL,
+  OPEN_EXISTING,
+  0,
+  NULL); 
+if (hDevice == INVALID_HANDLE_VALUE) // cannot open the drive,usually caused by permissions(UAC)
+{
+   printf("ERROR! Cannot access the drive!");  //Man! Rerun this program with Admin privilege!!!Don't forget to turn off your anti-virus!!!
+   return 2;
+}
+BYTE mbr[0x200] = {0};
+DWORD dwBeRead = 0;
+BOOL bRet = ReadFile(hDevice, &mbr, 0x200, &dwBeRead, NULL);
+if(bRet && dwBeRead)
+{
+  FILE *fp = fopen(MBRBKFILE, "wb+");//Write to file
+  if(fp)
+  {
+  int iBeWrite = fwrite(&mbr, sizeof(BYTE), 0x200, fp);
+  fclose(fp);
+  printf("MBR data was saved to %s",MBRBKFILE);//Complete!!!And you'll find the file ended with "55 AA"(In hex).If not, that's means backup process failed.
+ }
+}
+else
+{
+ DWORD err = GetLastError();
+}
+CloseHandle(hDevice);
+return 0;
+}
