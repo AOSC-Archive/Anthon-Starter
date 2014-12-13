ee15dbc5bcbed5448fba8625d92eb3802e696810
lmy441900 <826531041@qq.com>
2014-12-14 00:20:56 +0800

    Implement new standard of (osimage)/md5sum
    And maintain a compatibility with the old md5sum.ast

--- src/init.c
+++ src/init.c
@@ -17,68 +17,184 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 # include "ast.h"
-
 
 int init ( img *imginfo, char *osimage, char *ostarget )
 {
     char *tmp = NULL, *temp = getenv ( "TEMP" );
-    FILE *sum = NULL;
+    FILE *sumf = NULL;
     
+    /* First check if 7-Zip exists :) */
     if ( access ( "res\\7z.exe", X_OK ) == 0 )
     {
-        tmp = malloc ( 64 + strlen ( osimage ) ); /* I can't be bothered to allocate accurate memory. */
-        snprintf ( tmp, CMD_BUF, "%s %s %s%s %s%c", "res\\7z.exe x", osimage, "-o", "%temp%\\", "md5sum.ast -y > nul", '\0' ); /* NOTICE: ">nul" is not portable */
-        system ( tmp ); /* Extract md5sum.ast to %TEMP% */
-
-        snprintf ( tmp, CMD_BUF, "%s%s", temp, "\\md5sum.ast" );
-        if ( ( sum = fopen ( tmp, "rt" ) ) != NULL ) /* Open md5sum.ast as text, read only */
+        /* tmp: temporary command line buffer */
+        tmp = malloc ( CMD_BUF ); /* FIXME: I can't be bothered to allocate accurate memory :P */
+        
+        /* Execute res\7z.exe to extract the md5sum file inside */
+        /* New standard: (osimage)/md5sum */
+        
+        /* Output the command line to buffer (tmp) first... */
+        snprintf ( tmp, CMD_BUF, "%s %s %s%s %s%c", "res\\7z.exe x", osimage, "-o", "%temp%\\", "md5sum -y > nul", '\0' ); /* NOTICE: ">nul" is not portable */
+        /* Extract md5sum to %TEMP% */
+        system ( tmp ); /* WTF after 7-Zip failed it still returns 0! */
+        
+        /* So check the file's existence. */
+        snprintf ( tmp, CMD_BUF, "%s%s%c", temp, "\\md5sum", '\0' ); /* Put it in the buffer */
+        if ( access( tmp, R_OK ) == 0 )
         {
-            /* Memory allocation first */
-            imginfo -> dist           = malloc ( 5 ); /* anos'\0' */
-            imginfo -> ver            = malloc ( CMD_BUF );
-            imginfo -> lang           = malloc ( 6 ); /* en_US'\0' */
-            imginfo -> vmlinuz_chksum = malloc ( MD5SUM_LENGTH );
-            imginfo -> initrd_chksum  = malloc ( MD5SUM_LENGTH );
-            imginfo -> livesq_chksum  = malloc ( MD5SUM_LENGTH );
-
-            if ( fscanf ( sum, "%*[^\n] %*s os%1d %4s %512s %5s %32s %*s %32s %*s %32s",
-                                &(imginfo->os), imginfo->dist, imginfo->ver, imginfo->lang, /* FIXME: Some image files do not include "lang" field, and this will make mistakes in md5sum fields. */
-                                imginfo->vmlinuz_chksum, imginfo->initrd_chksum, imginfo->livesq_chksum ) == 7 )
+            /* md5sum exists and readable, obey the new standard */
+            snprintf ( tmp, CMD_BUF, "%s%s%c", temp, "\\md5sum", '\0' );
+            if ( ( sumf = fopen ( tmp, "rt" ) ) != NULL ) /* Open md5sum as text, read only */
             {
-                /* Examine the correction of the image information */
-                if ( ( imginfo->os == 0 ) || ( strlen ( imginfo->dist ) != 4 ) || ( strlen ( imginfo->ver ) == 0 ) ||
-                     ( strlen ( imginfo->lang ) == 0 ) || ( strlen ( imginfo->vmlinuz_chksum ) != 32 ) ||
-                     ( strlen ( imginfo->initrd_chksum ) != 32 ) || ( strlen ( imginfo->livesq_chksum ) != 32 ) )
+                /* Memory allocation first */
+                imginfo -> dist            = malloc ( 5 ); /* live'\0' */
+                imginfo -> ver             = malloc ( CMD_BUF );
+                imginfo -> vmlinuz_chksum  = malloc ( MD5SUM_LENGTH );
+                imginfo -> initrd_chksum   = malloc ( MD5SUM_LENGTH );
+                imginfo -> livesq_chksum   = malloc ( MD5SUM_LENGTH );
+                
+                /* Read-in */
+                char *ident = malloc ( 12 ); /* Temporary use: #ast-ident:'\0' */
+                while ( feof ( sumf ) == 0 )
                 {
-                    notify ( FAIL, "Error when reading md5sum.ast: It may not a supported AOSC OS.\n    Please contact us to get help. Program exits." );
-                    exit ( 1 );
-                }
-            }
+                    /* FIXME: A better reading method needed! */
+                    fscanf ( sumf, "%11s", ident );
+                    if ( strcmp ( ident, "#ast-ident:" ) == 0 ) /* #ast-ident: os3 live yyyymmdd */
+                    {
+                        if ( fscanf ( sumf, " os%1d %4s %8s%*[^\n]", /* 1 Space left still after last fscanf(), and skip the whole line. */
+                                            &(imginfo->os), imginfo->dist, imginfo->ver ) == 3 )
+                        {
+                            /* Verify whether they're valid or not */
+                            if (    ( imginfo->os == 0 )
+                                 || ( strcmp ( imginfo->dist, "live" ) != 0 )
+                                 || ( strlen ( imginfo->ver ) != 8 ) )
+                            {
+                                /* Not correct */
+                                notify ( FAIL, "Error when reading md5sum (ident): It may not a supported AOSC OS.\n    Please contact us to get help. Program exits." );
+                                exit ( 1 );
+                            }
+                            /* else: nothing to do! */
+                        } /* if ( fscanf ( sumf, ... ) */
+                        else
+                        {
+                            notify ( FAIL, "Not a correct md5sum file! Program exits." );
+                            exit ( 1 );
+                        }
+                    } /* if ( strcmp ( ident, "#ast-ident:" ) == 0 ) */
+                    else
+                    {
+                        if ( strcmp ( ident, "#md5sum" ) == 0 )
+                        {
+                            /* MD5 Checksums now reading */
+                            if ( fscanf ( sumf, "%32s %*s %32s %*s %32s",
+                                                imginfo->vmlinuz_chksum, imginfo->initrd_chksum, imginfo->livesq_chksum ) == 3 )
+                            {
+                                /* Verify whether they're valid or not */
+                                if (    ( strlen ( imginfo->vmlinuz_chksum ) != 32 )
+                                     || ( strlen ( imginfo->initrd_chksum  ) != 32 )
+                                     || ( strlen ( imginfo->livesq_chksum  ) != 32 ) )
+                                {
+                                    /* Not correct */
+                                    notify ( FAIL, "Error when reading md5sum (chksum): It may not a supported AOSC OS.\n    Please contact us to get help. Program exits." );
+                                    exit ( 1 );
+                                }
+                                /* else: of cource nothing to do! */
+                            }
+                            else
+                            {
+                                notify ( FAIL, "Not a correct md5sum file! Program exits." );
+                                exit ( 1 );
+                            }
+                        }
+                        else
+                            fscanf ( sumf, "%*[^\n]" ); /* Skip this line */
+                    }
+                } /* while ( feof ( sumf ) == 0 ) */
+                take ( ident );
+            } /* if ( ( sumf = fopen ( tmp, "rt" ) ) != NULL ) */
             else
             {
-                notify ( FAIL, "Failed to read md5sum.ast! Program exits." );
+                /* File in %TEMP% lost? WTF? */
+                notify ( FAIL, "Unknown error: File in %%temp%% not found. Program exits." );
                 exit ( 1 );
             }
-
-            notify ( INFO, "Image info:\n      os    : %d\n      dist  : %s\n      ver   : %s\n      lang  : %s\n      vmlchk: %s\n      inichk: %s\n      livchk: %s\n", imginfo->os, imginfo->dist, imginfo->ver, imginfo->lang, imginfo->vmlinuz_chksum, imginfo->initrd_chksum, imginfo->livesq_chksum );
-
-            fclose ( sum );
-            /* FIXME: Coverity reports bug here? */
-            remove ( tmp ); /* Remove the md5sum file */
-            sum = NULL; /* FILE *sum */
-            take ( tmp );
-        }
+        } /* if ( access( tmp, R_OK ) == 0 ) */
         else
         {
-            notify ( FAIL, "This ISO image is not supported.\n" );
-            take ( tmp );
-            exit ( 1 );
-        }
-    }
+            /* (osimage)/md5sum not exists. Try old standard (md5sum.ast) */
+            snprintf ( tmp, CMD_BUF, "%s %s %s%s %s%c", "res\\7z.exe x", osimage, "-o", "%temp%\\", "md5sum.ast -y > nul", '\0' ); /* NOTICE: ">nul" is not portable */
+            system ( tmp );
+            
+            /* Check the file's existence */
+            snprintf ( tmp, CMD_BUF, "%s%s%c", temp, "\\md5sum.ast", '\0' ); /* Put it in the buffer */
+            if ( access ( tmp, R_OK ) == 0 )
+            {
+                /* md5sum.ast exists, obey the old standard */
+                snprintf ( tmp, CMD_BUF, "%s%s%c", temp, "\\md5sum.ast", '\0' );
+                if ( ( sumf = fopen ( tmp, "rt" ) ) != NULL ) /* Open md5sum.ast as text, read only */
+                {
+                    /* Memory allocation first */
+                    imginfo -> dist            = malloc ( 5 ); /* anos'\0' */
+                    imginfo -> ver             = malloc ( CMD_BUF );
+                    imginfo -> lang            = malloc ( 6 ); /* en_US'\0' */
+                    imginfo -> vmlinuz_chksum  = malloc ( MD5SUM_LENGTH );
+                    imginfo -> initrd_chksum   = malloc ( MD5SUM_LENGTH );
+                    imginfo -> livesq_chksum   = malloc ( MD5SUM_LENGTH );
+                
+                    /* Read-in */
+                    /* FIXME: A better reading method needed! */
+                    if ( fscanf ( sumf, "%*[^\n] %*s os%1d %4s %512s %5s %32s %*s %32s %*s %32s",
+                                        &(imginfo->os), imginfo->dist, imginfo->ver, imginfo->lang,
+                                        imginfo->vmlinuz_chksum, imginfo->initrd_chksum, imginfo->livesq_chksum ) == 7 )
+                    {
+                        /* Examine the correction of the image information */
+                        if (    ( imginfo->os == 0 )
+                             || ( strlen ( imginfo->dist ) != 4 )
+                             || ( strlen ( imginfo->ver ) == 0 )
+                             || ( strlen ( imginfo->lang ) == 0 )
+                             || ( strlen ( imginfo->vmlinuz_chksum ) != 32 )
+                             || ( strlen ( imginfo->initrd_chksum  ) != 32 )
+                             || ( strlen ( imginfo->livesq_chksum  ) != 32 ) )
+                        {
+                            /* Not correct */
+                            notify ( FAIL, "Error when reading md5sum.ast: It may not a supported AOSC OS.\n    Please contact us to get help. Program exits." );
+                            exit ( 1 );
+                        }
+                        /* else: nothing to do. That's good. */
+                    } /* if ( fscanf ( sumf, ... ) */
+                    else
+                    {
+                        notify ( FAIL, "Not a correct md5sum.ast file! Program exits." );
+                        exit ( 1 );
+                    }
+                } /* if ( ( sumf = fopen ( tmp, "rt" ) ) != NULL ) */
+                else
+                {
+                    /* File in %TEMP% lost? WTF? */
+                    notify ( FAIL, "Unknown error: File in %%temp%% not found. Program exits." );
+                    exit ( 1 );
+                }
+            } /* if ( access ( "%temp%\\md5sum.ast", R_OK ) == 0 ) */
+            else
+            {
+                /* Not new standard or old standard, there can't be other reasons: Trash. */
+                notify ( FAIL, "Fatal: This file is not supported. Program exits." );
+                exit ( 1 );
+            }
+        } /* End of old standard reading */
+
+        /* This will be changed when release. (Won't be so ugly) */
+        notify ( INFO, "Image info:\n      os    : %d\n      dist  : %s\n      ver   : %s\n      lang  : %s\n      vmlchk: %s\n      inichk: %s\n      livchk: %s\n", imginfo->os, imginfo->dist, imginfo->ver, imginfo->lang, imginfo->vmlinuz_chksum, imginfo->initrd_chksum, imginfo->livesq_chksum );
+
+        fclose ( sumf );
+        /* FIXME: Coverity reports bug here? */
+        remove ( tmp ); /* Remove the md5sum file */
+        sumf = NULL;    /* FILE *sumf */
+        take ( tmp );   /* Take the memory of buffer */
+    } /* if ( access ( "res\\7z.exe", X_OK ) == 0 ) */
     else
     {
+        /* 7z executable not found */
         notify ( FAIL, "Cannot find 7-Zip executable. Program exits.\n" );
         exit ( 1 );
     }
