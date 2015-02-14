37b83672805f64bb6470a898a3567a80e6a193ce
lmy441900 <826531041@qq.com>
2015-02-15 00:49:22 +0800

    Fix the lack of infomation in help_message()

--- src/help_message.c
+++ src/help_message.c
@@ -27,7 +27,7 @@ int help_message ( char *progname ) /* "progname" is argv[0] */
 <Commands>\n\
   install            Install the specified AOSC OS Live environment\n\
                        to your computer\n\
-  help               Show this help\n\
+  help               Show this help message\n\
   startup            Active startup (to clean up trashes)\n\
 \n\
 <Switches>\n\
@@ -40,13 +40,17 @@ int help_message ( char *progname ) /* "progname" is argv[0] */
                          mbr: Edit the MBR (Only for machines have MBR)\n\
                          gpt: Edit the ESP (Only for machines have GPT)\n\
                          nodeploy: DO NOT deploy boot loader (not recommend)\n\
+  --verbose, -v      To be verbose\n\
+  --quiet, -q        To be quiet if there\'s no error\n\
   --pause, -p        Automatically pause after operation\n\
   --reboot, -r       Reboot the system after operation\n\
+  --yes, -y          Always answers yes to all warnings\n\
   --no-verify        DO NOT verify the files (not recommend)\n\
   --no-extract       DO NOT extract the files (not recommend)\n\
-  --help, -h         Show help messages\n\
+  --help, -h         Show this help message\n\
 \n\
 Report bugs to https://bugs.anthonos.org\n\
             or https://github.com/AOSC-Dev/Anthon-Starter/issues\n", progname );
       return 0;
 }
+
